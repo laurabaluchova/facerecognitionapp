98b9a036bbe992e6c84964f2df94fc9646e8b54e
laura.baluchova <laura.menova@gmail.com>
2023-02-20 11:23:23 +0100

    app updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import SignIn from './Components/SignIn/SignIn';

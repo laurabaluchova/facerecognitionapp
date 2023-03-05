ed6b171807c416e5c5c417e93e2a055ecead77d8
laura.baluchova <laura.menova@gmail.com>
2023-03-05 14:08:03 +0100

    navbar styling started

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -6,10 +6,26 @@ export default function  Navigation ( {onRouteChange, isSignedIn, changeModule})
     
     if (isSignedIn) {
         return (
-            <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-                <ul style={{marginRight: 'auto', display: "flex"}}>
-                    <NavLink to="/facerecognition" onClick={() => changeModule('face-detection')} className='f3 link b dim pa2 pr4 pointer white pa2'>Face Recognition</NavLink>
-                    <NavLink to="/colorrecognition" onClick={() => changeModule('color-recognition')} className='f3 link dim b pa2 pointer white pa2'>Color Recognition</NavLink>
+            <nav style={{display: 'flex', justifyContent: 'flex-end', backgroundColor: '#7c25cd', alignItems: "flex-end"}}>
+                <ul style={{marginRight: 'auto', display: "flex"}}>                    
+                    <NavLink 
+                        to="/colorrecognition" 
+                        onClick={() => changeModule('color-recognition')} 
+                        className='colorrecognition f3 link dim pa2 pointer pa2 b'
+                        style={({ isActive }) => ({
+                            color: isActive ? '#7c25cd' : '#ffffff',
+                            background: isActive ? "#ffa500" : "#7c25cd"                        
+                          })}
+                    >Color Recognition</NavLink>
+                    <NavLink 
+                        to="/facerecognition" 
+                        onClick={() => changeModule('face-detection')} 
+                        className='f3 link dim pa2 pr4 pointer pa2 b'
+                        style={({ isActive }) => ({
+                            color: isActive ? '#7c25cd' : '#ffffff',
+                            background: isActive ? "#ffa500" : "#7c25cd"  
+                          })}
+                    >Face Recognition</NavLink>
                 </ul>  
                 <NavLink to="/signin" onClick={() => onRouteChange('signout')} className='f3 link dim underline pa2 pointer white'>Sign Out</NavLink>
             </nav>

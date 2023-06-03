42fb75bd09bc7dd6e518a3ec0a0338a2344d105a
laura.baluchova <laura.menova@gmail.com>
2023-06-03 13:00:23 +0200

    custom hook for input added to sign in

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -3,134 +3,139 @@ import jwt_decode from "jwt-decode";
 import ParticlesBg from 'particles-bg';
 import React, { useState } from 'react';
 import { Link, useNavigate } from "react-router-dom";
+import useInput from '../../hooks/use-input';
 
-function  SignIn ({loadUser, serverUrl, setUser, setIsGoogleUser, isLoading, setIsLoading, cursor, setCursor}) {
-  
-  const [signInEmail, setSignInEmail] = useState('');
-  const [signInPassword, setSignInPassword] = useState('');  
-  const [emailIsTouched, setEmailIsTouched] = useState(false);
- 
+function SignIn({ loadUser, serverUrl, setUser, setIsGoogleUser, isLoading, setIsLoading, cursor, setCursor }) {
+  const {
+    value: signInEmail,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    onValueChange: onEmailChange,
+    onValueBlur: onEmailBlur
+  } = useInput(value => value.trim() !== "" && value.includes("@"));
+
+  const {
+    value: signInPassword,
+    isValid: passwordIsValid,
+    hasError: passwordHasError,
+    onValueChange: onPasswordChange,
+    onValueBlur: onPasswordBlur
+  } = useInput(value => value.trim() !== "");
+
   const navigate = useNavigate();
 
-  const emailIsValid = signInEmail.trim() !== "";
-  const emailInvalid = !emailIsValid && emailIsTouched;
+  let formIsValid = false;
 
-  const onEmailChange = (event) => {
-    setSignInEmail(event.target.value); 
-  }; 
-    
-const onEmailBlur = (event) => {
-  setEmailIsTouched(true);  
-}
+  if (emailIsValid && passwordIsValid) {
+    formIsValid = true;
+  };
 
-  const onPasswordChange = (event) => {
-    setSignInPassword(event.target.value)    
-  }
-
-const onSubmitSignIn = (event) => {
-  event.preventDefault();
-  setEmailIsTouched(true);
-  if (emailIsValid && signInPassword.trim() !== "") {    
-    setIsLoading(true);
-    setIsGoogleUser(false);   ;
-    localStorage.setItem("isGoogleUser", false)
-    localStorage.setItem("input", "")
-    setCursor("wait")    
-    fetch(`${serverUrl}/signin`, {
-      method: 'post',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        email: signInEmail,
-        password: signInPassword
+  const onSubmitSignIn = (event) => {
+    event.preventDefault();
+    if (formIsValid) {
+      setIsLoading(true);
+      setIsGoogleUser(false);;
+      localStorage.setItem("isGoogleUser", false)
+      localStorage.setItem("input", "")
+      setCursor("wait")
+      fetch(`${serverUrl}/signin`, {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          email: signInEmail,
+          password: signInPassword
+        })
       })
-    })
-      .then(response => 
-        {if (response.status === 400) {
-          setIsLoading(false);
-          console.log("sign in does not work")
-          setCursor("default");
-        }
-          return response.json()})
-      .then(user => {
-        if (user.id) {
-          loadUser(user);         
-          setIsLoading(false);
-          setCursor("default");          
-          navigate("/colorrecognition")
-      }
-    })  
-    .catch(() => {    
-      
-  });
-  } else {      
-    console.log("provide your credentials")
-    return;
+        .then(response => {
+          if (response.status === 400) {
+            setIsLoading(false);
+            console.log("sign in does not work")
+            setCursor("default");
+          }
+          return response.json()
+        })
+        .then(user => {
+          if (user.id) {
+            loadUser(user);
+            setIsLoading(false);
+            setCursor("default");
+            navigate("/colorrecognition")
+          }
+        })
+        .catch(() => {
+
+        });
+    } else {
+      console.log("provide your credentials")
+      return;
+    }
   }
-}
-  return (       
+  return (
     <article className="br5 ba b--white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-3 center"
       style={{ cursor: cursor }}>
-    <ParticlesBg type="cobweb" bg={true} color="#5E2CA5" />     
-    <main className="pa4 white">
+      <ParticlesBg type="cobweb" bg={true} color="#5E2CA5" />
+      <main className="pa4 white">
         <div className="measure ">
-            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-                <div className="mt3">
-                    <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                    <input onChange={onEmailChange}
-                    onBlur ={onEmailBlur}
-                    className="pa2 input-reset ba bg-white purple hover-bg-purple hover-white w-100" 
-                    type="email" 
-                    name="email-address" 
-                    id="email-address" />
-                  {emailInvalid && <p className='purple'>Provide valid email</p>}
-                </div>
-      <div className="mv3">
-        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-        <input onChange={onPasswordChange} 
-        className="b pa2 input-reset purple ba bg-white hover-bg-purple hover-white w-100" 
-        type="password" 
-        name="password"  
-        id="password" />
-      </div>      
-    </fieldset>
-    <div className="">
-      <input onClick={onSubmitSignIn}
-      className="b ph3 pv2 input-reset ba b--white bw2 bg-transparent grow pointer f6 dib white hover-bg-purple" type="submit" value={isLoading ? "Loading..." : "Sign in" }
-      style={{ cursor: cursor }}/>
-    </div>
-    <div className="lh-copy mt3">
-      <Link to="/register" disabled={isLoading} className="f6 link hover-purple white db pointer mb3">Register</Link>      
-    </div>
-    <div>
-    <GoogleLogin 
-            onSuccess={credentialResponse => {
-              setIsGoogleUser(true);
-              localStorage.setItem("isGoogleUser", true);                            
-              let userDataToken = credentialResponse
-              console.log(userDataToken)
-              let decodedToken = jwt_decode(userDataToken.credential);              
-              navigate("/colorrecognition")
-              setUser({
-                id: '',
-                name: decodedToken.given_name,
-                email: '',
-                entries: "",
-                joined: ''
-              })
-              window.localStorage.setItem('name', decodedToken.given_name);            
-            }}
-         
-            onError={() => {
-              console.log('Login Failed');
-            }}
-            useOneTap
-          
-          />
-    </div>
-  </div>
-</main>
-</ article>        
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+              <input onChange={onEmailChange}
+                onBlur={onEmailBlur}
+                className="pa2 input-reset ba bg-white purple hover-bg-purple hover-white w-100"
+                type="email"
+                name="email-address"
+                id="email-address" />
+              {emailHasError && <p className='purple'>Provide valid email</p>}
+            </div>
+            <div className="mv3">
+              <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+              <input onChange={onPasswordChange}
+                onBlur={onPasswordBlur}
+                className="b pa2 input-reset purple ba bg-white hover-bg-purple hover-white w-100"
+                type="password"
+                name="password"
+                id="password" />
+              {passwordHasError && <p className='purple'>Field cannot be empty</p>}
+            </div>
+          </fieldset>
+          <div className="">
+            <input onClick={onSubmitSignIn}
+              className="b ph3 pv2 input-reset ba b--white bw2 bg-transparent grow pointer f6 dib white hover-bg-purple" type="submit" value={isLoading ? "Loading..." : "Sign in"}
+              style={{ cursor: cursor }} />
+          </div>
+          <div className="lh-copy mt3">
+            <Link to="/register" disabled={isLoading} className="f6 link hover-purple white db pointer mb3">Register</Link>
+          </div>
+          <div>
+            <GoogleLogin
+              onSuccess={credentialResponse => {
+                setIsGoogleUser(true);
+                localStorage.setItem("isGoogleUser", true);
+                let userDataToken = credentialResponse
+                console.log(userDataToken)
+                let decodedToken = jwt_decode(userDataToken.credential);
+                navigate("/colorrecognition")
+                setUser({
+                  id: '',
+                  name: decodedToken.given_name,
+                  email: '',
+                  entries: "",
+                  joined: ''
+                })
+                window.localStorage.setItem('name', decodedToken.given_name);
+              }}
+
+              onError={() => {
+                console.log('Login Failed');
+              }}
+              useOneTap
+
+            />
+          </div>
+        </div>
+      </main>
+    </ article>
   );
-  }
+}
 export default SignIn;

c6824bc5d95a92fb81a78c01c3abd71f89a5999d
laura.baluchova <laura.menova@gmail.com>
2023-06-05 14:16:34 +0200

    cursor state started

--- src/Components/ImageLInkForm/ImageLinkForm.jsx
+++ src/Components/ImageLInkForm/ImageLinkForm.jsx
@@ -1,44 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from "react-router-dom";
 import './ImageLinkForm.css';
 import useInput from '../../hooks/use-input';
 import LoadingContext from '../../store/loading-context';
 
-const  ImageLinkForm = ({onInputChange, onSubmit, module, input, cursor}) => {   
-    let isGoogleUserLocalStorage = window.localStorage.getItem('isGoogleUser') === "true"; 
-    
+const ImageLinkForm = ({ onInputChange, onSubmit, module, input, cursor }) => {
+  let isGoogleUserLocalStorage = window.localStorage.getItem('isGoogleUser') === "true";
+  let localStorage
+  const ctx = useContext(LoadingContext);
+
   return (
-    <LoadingContext.Consumer>
-      {(ctx) => {
-        return (
-          <div style={{ cursor: cursor }}>
-          {isGoogleUserLocalStorage
-          ?
-            <p className='f4'>          
-                To count your detected images&nbsp;
-                <Link to="/register" className='white' >create account here</Link>
-             </p>
-          : <p className='f4'>          
-                {`Detect ${module.name} in your pictures with this Magic Brain App`}
-            </p>}
-            <p className='f5 i'>
-                {`Insert image URL to detect ${module.name}`}
-            </p>
-            <div className='center '>
-                <div className='form center pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type='text' placeholder="Insert image URL here" value={input} onChange={onInputChange}  />
-                    <button className='button w-30 grow f4 link ph3 pv2 dib white bg-purple pointer'
-                    onClick={onSubmit}                              
-                    >{ctx.isLoading ? "Loading..." : "Detect" }
-                    </button>                
-                 </div>
-            </div>
-        </div>   
-        )
-      }}
-   
-    </LoadingContext.Consumer> 
-  )
+    <div style={{ cursor: cursor }}>
+      {isGoogleUserLocalStorage
+        ?
+        <p className='f4'>
+          To count your detected images&nbsp;
+          <Link to="/register" className='white' >create account here</Link>
+        </p>
+        : <p className='f4'>
+          {`Detect ${module.name} in your pictures with this Magic Brain App`}
+        </p>}
+      <p className='f5 i'>
+        {`Insert image URL to detect ${module.name}`}
+      </p>
+      <div className='center '>
+        <div className='form center pa4 br3 shadow-5'>
+          <input className='f4 pa2 w-70 center' type='text' placeholder="Insert image URL here" value={input} onChange={onInputChange} />
+          <button className='button w-30 grow f4 link ph3 pv2 dib white bg-purple pointer'
+            onClick={onSubmit}
+          >{ctx.isLoading ? "Loading..." : "Detect"}
+          </button>
+        </div>
+      </div>
+    </div>)
 }
 
 export default ImageLinkForm;
